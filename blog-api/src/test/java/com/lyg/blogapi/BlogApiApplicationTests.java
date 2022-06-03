14690503f036c87a3a3e64ee9eb5c3501b99415e
almost <3370084577@qq.com>
2022-06-03 16:55:40 +0800

    Initial commit

--- blog-api/src/test/java/com/lyg/blogapi/BlogApiApplicationTests.java
+++ blog-api/src/test/java/com/lyg/blogapi/BlogApiApplicationTests.java
@@ -17,6 +17,7 @@ class BlogApiApplicationTests {
         System.out.println("加油");
         System.out.println("加油");
         System.out.println("修改代码");
-         System.out.println("整合代码");
+        System.out.println("整合代码");
+        System.out.println("查找代码");
     }
 }
